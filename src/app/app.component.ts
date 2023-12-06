33be4dbf388e15c5ff91bbbd802bda42046a09f9
mohammed-zeebaree <mohammedzebari4@gmail.com>
2023-12-06 18:26:25 +0300

    init files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
+// app.component.ts
 import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'mohammedsubhiHW';
-}
+@Component({ 
+  selector: 'app-root', 
+  templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.css'] 
+}) 
+export class AppComponent { 
+  @ViewChild('m') m!: NgForm; // Ensure the reference is properly defined
+ 
+  programmingLanguages = ['JavaScript', 'Python', 'Java', 'C++']; 
+  experienceLevels = ['Beginner', 'Intermediate', 'Advanced']; 
+ 
+  submit = false; 
+  submittedUsername: string = ''; 
+  submittedExperience: string = ''; 
+ 
+  submitForm() { 
+    this.submit = true; 
+    if (this.m && this.m.form) { 
+      this.submittedUsername = this.m.form.value.username; 
+      this.submittedExperience = this.m.form.value.experience; 
+    } 
+  } 
+} 
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+import { NgModule } from '@angular/core'; 
+import { BrowserModule } from '@angular/platform-browser'; 
+import {FormsModule} from '@angular/forms'; 
+import { AppComponent } from './app.component'; 
+ 
+@NgModule({ 
+  declarations: [ 
+    AppComponent 
+  ], 
+  imports: [ 
+    BrowserModule,FormsModule 
+  ], 
+  providers: [], 
+  bootstrap: [AppComponent] 
+})  
+export class AppModule { } 
+ 
